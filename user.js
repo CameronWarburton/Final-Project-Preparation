595b2acd55b403878e63165ea6144018f837bf0c
Cameron <cameron.cw@outlook.com>
2023-12-04 14:50:05 +1000

    Finished

--- user.js
+++ user.js
@@ -6,7 +6,7 @@ async function onSearchChange(event) {
     renderPosts(id)
 }
 
-async function renderPosts(userId) {
+async function renderPosts(id) {
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
   const postsData = await posts.json();
   postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
